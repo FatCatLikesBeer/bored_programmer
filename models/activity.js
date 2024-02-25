093fc8d14da42297e05013ab0110fc4cfa876eba
Billy P <itisbilly@icloud.com>
2024-02-25 03:20:49 -0800

    Added DB models

---
+++ models/activity.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ActivitySchema = new Schema({
+  activity: { type: String, required: true },
+  description: { type: String, required: true },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  tag: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+});
+
+// Export model
+module.exports = mongoose.model("Activity", ActivitySchema);
