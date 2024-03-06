aae07550eeb316e8ec1ed0e9a7e30a0f8eb9b866
Billy P <itisbilly@icloud.com>
2024-03-06 01:38:00 -0800

    API response displaying on index. Needs fine tuning

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const asyncHandler = require('express-async-handler');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Bored Programmer API' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  fetch('http://localhost:3000/api/')
+    .then(response => response.json())
+    .then(data => {
+      res.render('index', {
+        title: "Bored Programmer API",
+        code: replaceStuff(JSON.stringify(data)),
+      })
+    })
+})
+);
 
 router.post('/', function(req, res, next) {
   res.send("This shouldn't do anyting, how did you get here?")
 });
+
+function replaceStuff(input) {
+  const result = input
+  .replace('{', '{\n\t')
+  .replace(/:/g, ': ')
+  .replace(',"des', ',\n\t"des')
+  .replace(',"cat', ',\n\t"cat')
+  .replace(',"tag', ',\n\t"tag')
+  .replace('}', '\n}')
+  .replace(/,"/, ', "');
+  return result;
+};
 
 module.exports = router;
